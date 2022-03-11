6f2f6e4372f752faedd831014f46156e7b1e1dbd
Felipe A <felipearede@gmail.com>
2022-03-11 11:51:01 -0300

    requisito 4 feito - exibe as categorys na home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import * as api from './services/api';
 import Home from './components/Home';
 
 function App() {
-  api.getCategories().then((categories) => { console.log(categories); });
+  // api.getCategories().then((categories) => { console.log(categories); });
   return (
     <BrowserRouter>
       <Switch>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import * as api from '../services/api';
 
 export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      category: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((categories) => {
+      this.setState({ category: categories });
+    });
+  }
+
   render() {
+    const { category } = this.state;
     return (
       <div>
         <input type="text" />
-        <h1 data-testid="home-initial-message">
+        <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
+        </h2>
+        { category.map((item) => (
+          <label htmlFor={ item.id } data-testid="category" key={ item.id }>
+            <input id={ item.id } type="radio" value={ item.name } name="category" />
+            {item.name}
+          </label>
+        ))}
       </div>
     );
   }
